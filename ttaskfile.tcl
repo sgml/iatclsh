d9825d72c125638d12e6191de50a61213e8ec0c7
pdt <pault.eg@googlemail.com>
2011-09-06 18:33:29 +0100

    updated for newer ttask

--- ttaskfile.tcl
+++ ttaskfile.tcl
@@ -2,7 +2,7 @@
 # ttaskfile for iatclsh 
 #
 
-# gcc settings
+# config variables for linux kill gcc
 config INC_PATH /usr/include/tcl8.5
 config LIB_PATH /usr/lib
 
@@ -16,19 +16,19 @@ task build {
 
 task clean {file delete -force build}
 
-# linux kill config
-set linuxKill [project gcc]
+# linux kill shared library
+set linuxKill [project add gcc]
 $linuxKill src -add lib/linux_kill.c
 $linuxKill build -buildDir build/libkill -incPath $INC_PATH \
         -libPath $LIB_PATH -lib tclstub8.5 -name libkill.so 
 
-# tar archive config
-set tar [project tar]
+# tar archive 
+set tar [project add tar]
 $tar src -srcDir ./tcl -add $src -srcDir ./build/libkill -add libkill.so
 $tar build -buildDir build/tar -name iatclsh.tar.gz
 
-# zip archive config
-set zip [project zip]
+# zip archive 
+set zip [project add zip]
 $zip src -srcDir ./tcl -add $src 
 $zip build -buildDir build/zip -name iatclsh.zip
 
