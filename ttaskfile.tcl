a57e31a8094e30570557bbb8d295c3b9816261be
pdt <pault.eg@googlemail.com>
2011-10-05 19:29:41 +0100

    updated for newer ttask

--- ttaskfile.tcl
+++ ttaskfile.tcl
@@ -9,26 +9,26 @@ config LIB_PATH /usr/lib
 set src {iatclsh.tcl app_if.tcl PrefsDlg.tcl}
 
 task build {
-    $linuxKill build
-    $tar build
-    $zip build
+    linuxKillLib build
+    tarArchive build
+    zipArchive build
 }
 
 task clean {file delete -force build}
 
 # linux kill shared library
-set linuxKill [project add gcc]
-$linuxKill src -add lib/linux_kill.c
-$linuxKill build -buildDir build/libkill -incPath $INC_PATH \
+project add linuxKillLib -type gcc
+linuxKillLib src -add lib/linux_kill.c
+linuxKillLib build -buildDir build/libkill -incPath $INC_PATH \
         -libPath $LIB_PATH -lib tclstub8.5 -name libkill.so 
 
 # tar archive 
-set tar [project add tar]
-$tar src -srcDir ./tcl -add $src -srcDir ./build/libkill -add libkill.so
-$tar build -buildDir build/tar -name iatclsh.tar.gz
+project add tarArchive -type tar
+tarArchive src -srcDir ./tcl -add $src -srcDir ./build/libkill -add libkill.so
+tarArchive build -buildDir build/tar -name iatclsh.tar.gz
 
 # zip archive 
-set zip [project add zip]
-$zip src -srcDir ./tcl -add $src 
-$zip build -buildDir build/zip -name iatclsh.zip
+project add zipArchive -type zip
+zipArchive src -srcDir ./tcl -add $src 
+zipArchive build -buildDir build/zip -name iatclsh.zip
 
